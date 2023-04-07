bda7e2e0080524a71973ee315524b0347902c347
JacobJax <jacobjax617@gmail.com>
2023-04-07 12:51:39 +0300

    change db

--- Final_Project_part1/db/db.php
+++ Final_Project_part1/db/db.php
@@ -2,7 +2,7 @@
 
     function establishCONN() {
         // dev_db
-        $pdo = new PDO('mysql:host=localhost;port=3306;dbname=res_deb', 'root', '');
+        $pdo = new PDO('mysql:host=localhost;port=3306;dbname=reservation', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         return $pdo;
